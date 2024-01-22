9f69fc7e6bbafd615e01a2965f5124c641c6486f
Alessandro Proto <alex@alexdevs.me>
2024-01-22 18:05:01 +0100

    Add project files.

---
+++ SoundTouched/AudioControllerPatch.cs
@@ -0,0 +1,101 @@
+﻿using Audio;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Networking;
+
+namespace SoundTouched;
+
+[HarmonyPatch(typeof(AudioController))]
+class AudioControllerPatch
+{
+    internal static Dictionary<string, string> AudioFiles = [];
+
+    [HarmonyPatch("Awake")]
+    [HarmonyPostfix]
+    static void AwakePatch(AudioController __instance, ref AudioComponent[] ____audioComponents)
+    {
+
+        Plugin.Logger.LogInfo("This scene has the following audio tracks:");
+        foreach (var audio in ____audioComponents)
+        {
+            Plugin.Logger.LogInfo($" - {audio.Name}");
+        }
+    }
+
+    internal static void PatchAudioClip(AudioComponent component, AudioClip audioClip)
+    {
+        Plugin.Logger.LogInfo($"Patching sound: {component.Name}");
+        var originalAudioObjectTraverse = Traverse.Create<AudioComponent>().Field<AudioSource>("Audio");
+        UnityEngine.Object.Destroy(originalAudioObjectTraverse.Value, 0f);
+        var audioSource = component.gameObject.AddComponent<AudioSource>();
+        originalAudioObjectTraverse.Value = audioSource;
+        component.GetType().GetField("Audio", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(component, audioSource);
+        audioSource.clip = audioClip;
+        component.GetType().GetField("PitchMinModifier", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(component, 1f);
+        component.GetType().GetField("PitchMaxModifier", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(component, 1f);
+    }
+}
+
+[HarmonyPatch(typeof(SfxComponent))]
+class SfxComponentPatch
+{
+    [HarmonyPatch("Awake")]
+    [HarmonyPostfix]
+    static void AwakePatch(SfxComponent __instance, ref AudioSource ___Audio)
+    {
+        var wavFile = Plugin.WavFiles.FirstOrDefault(p => Path.GetFileNameWithoutExtension(p) == __instance.Name);
+        if (wavFile == null)
+            return;
+
+        var volume = ___Audio.volume;
+        Plugin.Logger.LogInfo($"Loading audio file {wavFile}");
+
+        var audioClip = Plugin.GetAudioClip(wavFile, AudioType.WAV);
+        AudioControllerPatch.PatchAudioClip(__instance, audioClip);
+        ___Audio.volume = volume;
+    }
+}
+
+[HarmonyPatch(typeof(BgmComponent))]
+class BgmComponentPatch
+{
+    [HarmonyPatch("Awake")]
+    [HarmonyPostfix]
+    static void AwakePatch(SfxComponent __instance, ref AudioSource ___Audio, ref bool ____hasIntro, AudioSource ____introAudioSource)
+    {
+        var wavFile = Plugin.WavFiles.FirstOrDefault(p => Path.GetFileNameWithoutExtension(p) == __instance.Name);
+        if (wavFile == null)
+            return;
+
+
+        var volume = ___Audio.volume;
+        Plugin.Logger.LogInfo($"Loading audio file {wavFile}");
+
+        var audioClip = Plugin.GetAudioClip(wavFile, AudioType.WAV);
+        AudioControllerPatch.PatchAudioClip(__instance, audioClip);
+        ___Audio.volume = volume;
+
+        ____hasIntro = false;
+
+        if (____hasIntro)
+        {
+            /*var dirPath = Path.GetDirectoryName(wavFile);
+            var extension = Path.GetExtension(wavFile);
+            var introFile = $"{__instance.Name}_Intro{extension}";
+
+            ____introAudioSource.volume = volume;*/
+        }
+
+        // TODO: add logic for _introAudioSource
+    }
+}
---
+++ SoundTouched/Plugin.cs
@@ -0,0 +1,60 @@
+﻿using Audio;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace SoundTouched;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger { get; private set; }
+    public static string SoundsPath { get; private set; }
+    public static List<string> WavFiles { get; private set; } = [];
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+
+        var pluginPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var SoundsPath = Path.Combine(pluginPath, "sounds");
+        if (!Directory.Exists(SoundsPath))
+            Directory.CreateDirectory(SoundsPath);
+        WavFiles = Directory.EnumerateFiles(SoundsPath)
+            .Where(q => q.ToLower().EndsWith(".wav")).ToList();
+
+        Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+    }
+
+    public static AudioClip GetAudioClip(string path, AudioType audioType = AudioType.UNKNOWN)
+    {
+        AudioClip audioClip = null;
+        using var uwr = UnityWebRequestMultimedia.GetAudioClip(path, audioType);
+        uwr.SendWebRequest();
+        try
+        {
+            while (!uwr.isDone)
+                Task.Delay(5).Wait();
+
+            if (uwr.result == UnityWebRequest.Result.ConnectionError || uwr.result == UnityWebRequest.Result.ProtocolError)
+                Logger.LogError($"{uwr.error}");
+            else
+                audioClip = DownloadHandlerAudioClip.GetContent(uwr);
+        }
+        catch (Exception err)
+        {
+            Logger.LogError($"{err.Message}, {err.StackTrace}");
+        }
+
+
+        return audioClip;
+    }
+}
